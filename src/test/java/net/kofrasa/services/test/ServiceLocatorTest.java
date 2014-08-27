d05ea6b0571249a365b9e31bc2c41274ca394627
Francis Asante <kofrasa@gmail.com>
2014-08-27 13:43:13 +0000

    added tests and bug fixes

--- src/test/java/net/kofrasa/services/test/ServiceLocatorTest.java
+++ src/test/java/net/kofrasa/services/test/ServiceLocatorTest.java
@@ -1,8 +1,9 @@
 package net.kofrasa.services.test;
 
-import net.kofrasa.services.ServiceFactory;
 import net.kofrasa.services.ServiceLocator;
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import java.util.Date;
 import java.util.Properties;
@@ -15,23 +16,47 @@ import java.util.Random;
  */
 public class ServiceLocatorTest {
 
-    public void setUp() {
+    @Before
+    public void setUp() throws Exception {
         Properties props = new Properties();
         props.setProperty("random", "java.util.Random");
+        props.setProperty("randomFactory", "net.kofrasa.services.test.RandomFactory");
         ServiceLocator.instance.init(props);
         ServiceLocator.instance.register("date", new Date());
     }
 
     @Test
     public void testRegisterValues() {
-
+        Date first = ServiceLocator.instance.value("date");
+        Date second = ServiceLocator.instance.value("date");
+        assertEquals(first, second);
     }
-}
 
+    @Test
+    public void testNewInstances() {
+        Random first = ServiceLocator.instance.create("random");
+        Random second = ServiceLocator.instance.create("random");
+        assertNotSame(first, second);
+    }
 
-class RandomFactory implements ServiceFactory<Random> {
-    @Override
-    public Random newInstance() {
-        return new Random();
+    @Test
+    public void testSingletons() {
+        Random first = ServiceLocator.instance.singleton("random");
+        Random second = ServiceLocator.instance.singleton("random");
+        assertEquals(first, second);
+    }
+
+    @Test
+    public void testServiceFactorySingleton() {
+        Random first = ServiceLocator.instance.singleton("randomFactory");
+        Random second = ServiceLocator.instance.singleton("randomFactory");
+        assertEquals(first, second);
+    }
+
+    @Test
+    public void testServiceFactoryInstances() {
+        Random first = ServiceLocator.instance.create("randomFactory");
+        Random second = ServiceLocator.instance.create("randomFactory");
+        assertNotSame(first, second);
     }
 }
